b54c5164b1b319283f26e92dc2c4e99bea0d067b
zahidhai76 <45167319+zahidhai76@users.noreply.github.com>
2022-05-02 16:39:20 +0100

    Add files via upload

---
+++ Tools.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Remove
+{
+    private int n;
+    private string word;
+
+    public int N
+    {
+        get { return n; }
+        set { n = value; }    
+    }
+
+    public string Word
+    {
+        get { return word; }
+        set { word = value; }
+    }
+
+    public void removal(string word, int n)
+    {
+        Console.WriteLine(word.Remove(n, 1));
+    }
+}
