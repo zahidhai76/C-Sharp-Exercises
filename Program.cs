7a4e0da6cb61bc6baa9d9ed8e3c384f07d251f86
zahidhai76 <45167319+zahidhai76@users.noreply.github.com>
2022-05-05 22:19:29 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1260,5 +1260,83 @@ namespace Calc
 
     }
 
+    public class Remove
+    {
+        private int n;
+        private string word;
+
+        public int N
+        {
+            get { return n; }
+            set { n = value; }
+        }
+
+        public string Word
+        {
+            get { return word; }
+            set { word = value; }
+        }
+
+        public void removal(string word, int n)
+        {
+            Console.WriteLine(word.Remove(n, 1));
+        }
+    }
+
+    public class Swap
+    {
+        private string word;
+        public string Word
+        {
+            get { return word; }
+            set { word = value; }
+        }
+        //  public string Word
+        //  { get; set; }
+        public void CharacterSwap(string word)
+        {
+            Console.WriteLine(word.Length > 1 ? word.Substring(word.Length - 1) + word.Substring(1, word.Length - 2) + word.Substring(0, 1) : word);
+        }
+    }
+
+    public class wordInt
+    {
+        private string word;
+        private int n;
+
+        public string Word
+        {
+            get { return word; }
+            set { word = value; }
+        }
+
+        public int N
+        {
+            get { return n; }
+            set
+            {
+                if (value > 0)
+                {
+                    n = value;
+                }
+                else
+                {
+                    n = 0;
+                }
+            }
+        }
+
+        public void copies()
+        {
+            string result = "";
+            for (int i = 0; i < n; i++)
+            {
+                result += word;
+            }
+            Console.WriteLine(result);
+        }
+
+    }
+
 }
 
